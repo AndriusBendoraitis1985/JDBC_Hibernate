1ce681354d4c37cd0a505b3fc4a767d8dacb2c46
AndriusBendoraitis <andrius.bendoraitis1985@gmail.com>
2020-04-25 17:13:37 +0300

    do not work upload list of data. Uploading only last one

---
+++ src/main/java/exercises/JDBC/Exercise1/Main.java
@@ -0,0 +1,55 @@
+package exercises.JDBC.Exercise1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        List<Person> personList = getPersonsList();
+
+        uploadToDataBase(personList);
+    }
+
+    private static List<Person> getPersonsList() {
+        List<Person> personList = new ArrayList<Person>();
+        Person p1 = new Person("Miroslav", "Cerniuk");
+        Person p2 = new Person("Inga", "Cerniuk");
+        personList.add(p1);
+        personList.add(p2);
+        return personList;
+    }
+
+    private static void uploadToDataBase(List<Person> personList) throws Exception {
+        String url = "jdbc:mysql://localhost:3306/testDB";
+        String userName = "root";
+        String password = "root";
+        String querry = "Insert into persons (first_name, last_name) values (?,?);";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, userName, password);
+        java.sql.PreparedStatement st = con.prepareStatement(querry);
+
+/*
+        for (Person person:personList) {
+            st.setString(1,person.getName());
+            st.setString(2,person.getSurname());
+            querry = "Insert into persons (first_name, last_name) values (?,?);";
+        }
+*/
+        st.setString(1, personList.get(0).getName());
+        st.setString(2, personList.get(0).getSurname());
+
+        st = con.prepareStatement(querry);
+
+        st.setString(1, personList.get(1).getName());
+        st.setString(2, personList.get(1).getSurname());
+
+        int count = st.executeUpdate();
+        System.out.println(count + " row/s affected");
+        st.close();
+        con.close();
+    }
+}
